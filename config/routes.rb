3ba645cba51a82bbcd828b2d81e51fde75e60b3b
Josh McLinton <jjlumclinton@gmail.com>
2018-06-27 22:52:34 +0100

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments
+
+    resources :posts do
+    	resources :comments
+    end
+
+    root "posts#index"
+
+    get '/about', to: 'pages#about'
+
   end
-  
-  root 'posts#index'
-end
